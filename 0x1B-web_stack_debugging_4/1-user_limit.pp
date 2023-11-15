cb9bba40b1dbafec57f31233613ab6531bff42fd
chenebuah-david <davidchenebuah@gmail.com>
2023-11-15 12:11:14 -0800

    Task 0 - 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+# This will allow the user holberton to login and open files without error
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# The soft file limit for Holberton user will increase
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,1 @@
+This is the 0x1B-web stack debugging 4 project
