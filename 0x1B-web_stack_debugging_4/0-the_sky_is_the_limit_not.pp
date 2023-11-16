3787df2647c26fde9a26b746980b9f41ded9adbd
chenebuah-david <davidchenebuah@gmail.com>
2023-11-16 07:47:37 -0800

    Task 0 - 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# The amount of traffic an Nginx server can handle will increase
-# The ULIMIT of the default file will increase
+# The amount of traffic an Nginx server can handle will increase
+# The ULIMIT of the default file will insrease
 
-exec { 'fix--for-nginx': 
-  command => 'sed -i "s/15/4096/" /etc/default/nginx', 
-  path    => '/usr/local/bin/:/bin/' 
-} -> 
-  
-# Nginx restarts
-exec { 'nginx-restart': 
-  command => 'nginx restart', 
-  path    => '/etc/init.d/' 
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Nginx restarts
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
